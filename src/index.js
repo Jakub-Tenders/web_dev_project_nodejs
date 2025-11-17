c71d6f67282b720f72aa4813bc27f75b66bb5dd1
Jakub Motysia <jakub.motysia@epita.fr>
2025-11-17 12:23:06 +0100

    edit

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ app.use(logMiddleware);
 app.use(express.static(path.join(__dirname, "frontend")));
 
 
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "index.html")); // ADDED
 });
